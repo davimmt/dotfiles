b8f90cabcddc7103211f71eb3664a698c5b6055a
Davi Tavares <mirandadavi.mt@gmail.com>
2023-08-10 10:24:20 -0300

    ADD mapping to yank/move lines up/down

--- nvim/nvchad/mappings.lua
+++ nvim/nvchad/mappings.lua
@@ -4,6 +4,10 @@ M.general = {
   n = {
     ["Q"] = { ":q!<CR>", "Quit" },
     ["<leader>gg"] = { ":LazyGit<CR>", "LazyGit" },
+    ["<A-Up>"] = { "yyP", "Yank line up" },
+    ["<A-Down>"] = { "yyp", "Yank line down" },
+    ["<C-A-Up>"] = { ":m -2<CR>", "Move line up" }, -- "ddkP" deletes line if on top
+    ["<C-A-Down>"] = { "ddp", "Move line down" },
   },
 }
 
