6b41e18c71a8e589558a7741c4d321e4429dbd9d
Davi Tavares <mirandadavi.mt@gmail.com>
2024-06-25 15:31:25 -0300

    fix(nix-shell): locale

--- .shell.nix
+++ .shell.nix
@@ -1,7 +1,9 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
   packages = with pkgs; [
+    ##################################
     # utils
+    ##################################
     neovim
     nodejs # copilot
     git
@@ -11,13 +13,17 @@ pkgs.mkShell {
     ripgrep
     fzf
     zoxide
+    ##################################
     # shell
+    ##################################
     zsh
     oh-my-zsh
     oh-my-posh
     zsh-powerlevel10k
     antigen
+    ##################################
     # devops
+    ##################################
     awscli2
     kubectl
     #terraform # NIXPKGS_ALLOW_UNFREE=1
@@ -27,8 +33,14 @@ pkgs.mkShell {
     checkov
   ];
 
+  NIX_FZF = "${pkgs.fzf}";
+  NIX_ANTIGEN = "${pkgs.antigen}";
 
-shellHook = ''
-  export NIX_FZF=${pkgs.fzf} NIX_ANTIGEN=${pkgs.antigen}
-'';
+  ##################################
+  # fix for perl locale error
+  LANGUAGE = "C";
+  LC_ALL = "C.UTF-8";
+  LANG = "C.UTF-8";
+  LC_CTYPE = "C.UTF-8";
+  ##################################
 }
